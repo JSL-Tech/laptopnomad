3d4b0ee18d0a2657e80154cc6a5189d8dd291f7c
Lebarnon <53988015+Lebarnon@users.noreply.github.com>
2021-05-28 11:00:05 +0800

    added env variables
    firebase keys are not .env variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,15 @@ export default {
     [
       '@nuxtjs/firebase',
       {
-        config: firebaseConfig,
+        config: {
+          apiKey: process.env.API_KEY,
+          authDomain: process.env.AUTH_DOMAIN,
+          projectId: process.env.PROJECT_ID,
+          storageBucket:process.env.STORAGE_BUCKET,
+          messagingSenderId: process.env.MESSAGING_SENDER_ID,
+          appId: process.env.APP_ID,
+          measurementId: process.env.MEASUREMENT_ID
+        },
         services: {
           auth: true,
           firestore: true,
