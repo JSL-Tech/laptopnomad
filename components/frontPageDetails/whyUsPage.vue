64758269b5cf2ae91ea907e185d23f0b9a804235
JosephJL <HeeJLJoseph@gmail.com>
2021-06-14 09:57:58 +0800

    Create whyUsPage.vue

---
+++ components/frontPageDetails/whyUsPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="whyUs" ref="whyUs" id="whyUs" >
+        <div class="whyUs">
+            <div>
+                <b-img src="../../assets/images/Advert.png" fluid alt="Responsive image" class="card__image card__image--1"></b-img>
+            </div>
+        </div>
+    </div>
+  
+</template>
+
+<script>
+import { defineComponent } from "@nuxtjs/composition-api";
+
+export default defineComponent ({
+    setup () {
+        
+    }, 
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
